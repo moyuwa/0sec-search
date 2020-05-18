435cf0e81707a414f77d06620283110116a29868
时光难逆 <31231318+moyuliu@users.noreply.github.com>
2020-05-18 15:03:56 +0800

    上传项目文件
    vs_0sec.py 运行文件
    tree.json 漏洞名json （20200518）
    .db 漏洞信息数据库

---
+++ vs_0sec.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+# coding=utf-8
+# python version 3.7
+# 零组资料文库离线漏洞名搜索
+
+import os, sys
+import json, sqlite3, time
+import requests
+
+
+# 功能：更新 、查询
+class search_0sec():
+    def __init__(self):
+        self._db_name = 'vulsearch-database.db'  # 数据库文件名
+        self._table_name = '0sec'
+        if os.path.exists(self._db_name) and os.path.isfile(self._db_name):
+            pass
+        else:
+            print('未找到数据库文件vulsearch-database.db，将自动创建空库')
+
+    # 下载漏洞json到本地
+    def downloadjson(self):
+        u = 'https://wiki.0-sec.org/api/wiki/tree'
+        # 循环直到获取成功（有时下载不成功）
+        while 1:
+            try:
+                _res = requests.get(u, verify=False)
+                if _res.status_code == 200:
+                    json.loads(_res.text)  # 能被json解析表示数据完整
+                    with open('tree.json', 'w', encoding='utf-8') as f:
+                        f.write(_res.text)
+                    break
+            except:
+                time.sleep(3)
+
+    # 将json转存到sqlit3数据库便于查询
+    def dump2sqlit3(self):
+        # 创建库
+        _conn = sqlite3.connect(self._db_name)
+        # 创建表,只保留2级目录
+        qy = """CREATE TABLE IF NOT EXISTS "%s"(
+           count INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+           name1 TEXT,name2 TEXT,data TEXT);"""
+        _conn.execute(qy % self._table_name)
+        qy = """delete from "%s";"""
+        _conn.execute(qy % self._table_name)  # 清空表（偷懒）
+        qy = """update sqlite_sequence SET seq = 0 where name ='%s';"""
+        _conn.execute(qy % self._table_name)  # 清空自增长
+        # 读取数据
+        with open('tree.json', 'r', encoding='utf-8') as f:
+            data = json.load(f)
+        # 获取漏洞列表
+        for v1 in data['data']:  # 输出大类别
+            name1 = v1['name']
+            for v2 in v1['treeNode']:  # 输出小类别
+                name2 = v2['name']
+                vul_list = self.jsontreeNode(v2)
+                # 将漏洞信息存到数据库
+                cur = _conn.cursor()
+                qy_config = "INSERT INTO '%s'(name1,name2,data)VALUES(?,?,?);" % self._table_name
+                for vinfo in vul_list:
+                    v = (str(name1), str(name2), str(vinfo))
+                    cur.execute(qy_config, v)
+                _conn.commit()
+        _conn.close()
+
+    # 从2级节点中循环提取子节点
+    def jsontreeNode(self, v2=json):
+        result = []
+        v3_list = v2['treeNode']
+        if type(v3_list) is not list:  # 直到没有子节点，返回漏洞信息
+            result.append(v2)
+            return result
+        else:  # 如果有子节点就循环获取内部信息
+            for v3 in v3_list:
+                result.extend(self.jsontreeNode(v3))
+        return result
+
+    # 查询数据库返回格式化结果
+    def searchsqlit3(self, keyword=str):
+        _conn = sqlite3.connect(self._db_name)
+        _nodes = list(_conn.execute("SELECT * FROM '{0}' WHERE data like '%{1}%'".format(self._table_name, keyword)))
+        for node in _nodes:
+            vinfo = eval(node[3])
+            # 根据命令规则拼接url
+            http_h = 'https://wiki.0-sec.org/api/wiki/articleInfo/' + str(vinfo['id'])
+            name = node[1] + '|' + node[2] + '|' + vinfo['name']
+            print('{0}\t{1}'.format(name, http_h))
+        _conn.close()
+
+
+if __name__ == '__main__':
+    if sys.argv.__len__() < 2:
+        print("""零组资料文库离线漏洞名搜索,功能：更新 、查询
+        -up:更新数据
+        -s [keyword]:查询关键字
+        """)
+        exit(0)
+    _0sec = search_0sec()
+    if sys.argv[1] == '-up':
+        _0sec.downloadjson()
+        _0sec.dump2sqlit3()
+        print('更新完成')
+    if sys.argv[1] == '-s':
+        _0sec.searchsqlit3(sys.argv[2])
