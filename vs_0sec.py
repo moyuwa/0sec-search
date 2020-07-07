1b0f2fa484aba64fd526471f87c890dee8f27784
时光难逆 <31231318+moyuwa@users.noreply.github.com>
2020-07-07 11:24:09 +0800

    Add files via upload

--- vs_0sec.py
+++ vs_0sec.py
@@ -92,7 +92,8 @@ class search_0sec():
 if __name__ == '__main__':
     if sys.argv.__len__() < 2:
         print("""零组资料文库离线漏洞名搜索,功能：更新 、查询
-        -up:更新数据
+        -up:在线更新数据
+        -offline:离线更新数据库（根据tree.json文件更新）
         -s [keyword]:查询关键字
         """)
         exit(0)
